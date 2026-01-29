0f5ed01e8ad9a042015105c1262d4b5ea0219b50
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2026-01-29 18:57:32 -0500

    Duplicate campaign entry in campaignRegistry

--- D20 - Website/campaigns.js
+++ D20 - Website/campaigns.js
@@ -8,6 +8,15 @@ const campaignRegistry = [
         description: "An epic musical adventure across the cosmos",
         banner: "https://raw.githubusercontent.com/Kiderobin/D20/refs/heads/main/D20-Official%20Campaigns/Starlight%20Symphony/banner.png",
         infoUrl: "https://raw.githubusercontent.com/Kiderobin/D20/refs/heads/main/D20-Official%20Campaigns/Starlight%20Symphony/info.js"
+    },
+    {
+        name: "Starlight Symphony",
+        color: "#8a2be2",
+        folder: "Starlight Symphony",
+        description: "An epic musical adventure across the cosmos",
+        banner: "https://raw.githubusercontent.com/Kiderobin/D20/refs/heads/main/D20-Official%20Campaigns/Starlight%20Symphony/banner.png",
+        infoUrl: "https://raw.githubusercontent.com/Kiderobin/D20/refs/heads/main/D20-Official%20Campaigns/Starlight%20Symphony/info.js"
     }
     // Add more campaigns here
 ];
+
