a73c8d49e799ab0cbf0abcfadf659a825e220e5c
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2026-01-29 17:39:56 -0500

    Create initial campaign registry in Campaigns.js
    Initialized campaign registry with a sample campaign.

---
+++ D20-Official Campaigns/Campaigns.js
@@ -0,0 +1,11 @@
+// Central registry of all campaigns
+// Add new campaigns here when you create them
+const campaignRegistry = [
+    {
+        name: "Starlight Symphony",
+        folder: "Starlight Symphony",
+        description: "An epic musical adventure across the cosmos",
+        banner: ,
+    }
+    // Add more campaigns here
+];
