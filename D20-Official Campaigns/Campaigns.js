1677008086e2087f2c9139b7fdf28e47c463a541
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2026-01-30 14:02:21 -0500

    Add 'Ashes Of War' campaign details

--- D20-Official Campaigns/Campaigns.js
+++ D20-Official Campaigns/Campaigns.js
@@ -6,6 +6,14 @@ const campaignRegistry = [
         folder: "Starlight Symphony",
         description: "An epic musical adventure across the cosmos",
         banner: ,
+    },
+    {
+        name: "Ashes Of War",
+        color: "#da120b",
+        folder: "AshesOfWar",
+        description: "The Third installment of The Baron Of Flame Story",
+        banner: "https://raw.githubusercontent.com/Kiderobin/D20/refs/heads/main/D20-Official%20Campaigns/Starlight%20Symphony/banner.png",
+        infoUrl: "https://raw.githubusercontent.com/Kiderobin/D20/refs/heads/main/D20-Official%20Campaigns/AshesOfWar/info.js"
     }
     // Add more campaigns here
 ];
