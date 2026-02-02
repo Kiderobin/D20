f858cdedf88f587be7a20edd5733653d7a95d2d2
Scarecrow <94719017+ScareTheVoices@users.noreply.github.com>
2026-02-02 00:10:57 -0500

    Add new campaign 'Anchors Away' to Campaigns.js

--- D20-Official Campaigns/Campaigns.js
+++ D20-Official Campaigns/Campaigns.js
@@ -14,6 +14,14 @@ const campaignRegistry = [
         description: "The Third installment of The Baron Of Flame Story",
         banner: "",
         infoUrl: "https://raw.githubusercontent.com/Kiderobin/D20/refs/heads/main/D20-Official%20Campaigns/AshesOfWar/info.js"
+    },
+      {
+        name: "Anchors Away",
+        color: "#3d9fb7",
+        folder: "AnchorsAway",
+        description: "Set Off On The Seven Seas and live a life as a pirate serve the world government with the Marines",
+        banner: "",
+        infoUrl: "https://raw.githubusercontent.com/ScareTheVoices/D20/refs/heads/main/D20-Official%20Campaigns/AnchorsAway/info.js"
     }
     // Add more campaigns here
 ];
