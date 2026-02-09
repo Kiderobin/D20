79ad50a8f97592b8480ecfc41e37b412156d2957
Scarecrow <94719017+ScareTheVoices@users.noreply.github.com>
2026-02-08 22:09:48 -0500

    Add files via upload

---
+++ D20-Official Campaigns/SoulRemnant/info.js
@@ -0,0 +1,53 @@
+const campaignInfo = {
+    // Basic information
+    title: "Soul Remnant",
+    subtitle: "What Remains After Death",
+    description: "An Urban Supernatural Campaign Based.",
+    
+    // Campaign metadata
+    players: "1-3",
+    duration: "1+",
+    difficulty: "Medium",
+    
+    // Banner image URL
+    banner: "https://raw.githubusercontent.com/ScareTheVoices/D20/refs/heads/main/D20-Official%20Campaigns/SoulRemnant/banner.png",
+    
+    // Story section (can include HTML)
+    story: `
+        <h3> Campaign Overview</h3>
+        <p>Whilst trying to uncover the existance of Ghosts for their Youtube Channel, The Player(s) stumbles into a Remnant's Domain, and as the clock stuck 11:59pm, they found themselves stuck in it's hunting grounds...
+        Once the players find themselves trapped, their only options are to fight to survive or except fate and perish at the hands of what's left behind when a Murderer passes and leaves behind their Remnant...</p>
+        
+        `,
+    
+    // Key features
+    features: [
+        "Anime Elements",
+        "Mystery and investigation",
+        "One Race & One Class",
+        "Urban Supernatural Setting",
+        "Work in progress"
+    ],
+    
+    // Campaign structure
+    chapters: [
+       "Epilogue: Remnant",
+    ],
+    
+    // Resources and downloads
+    resources: [
+        {
+            name: "Play",
+            link: "https://scarethevoices.github.io/D20/D20-Official%20Campaigns/SoulRemnant/D20.html"
+        }
+    ]
+};
+
+window.campaignInfo = campaignInfo;
+
+
+
+
+
+
+
