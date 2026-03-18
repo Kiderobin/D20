7d961d3130a52926bdef5f71a6a677912d2ebcc4
Scarecrow <94719017+ScareTheVoices@users.noreply.github.com>
2026-03-18 19:41:07 -0400

    Add files via upload

---
+++ D20-Official Campaigns/ShatteredTales/info.js
@@ -0,0 +1,51 @@
+const campaignInfo = {
+    // Basic information
+    title: "Shattered Tales",
+    subtitle: "",
+    description: "",
+    
+    // Campaign metadata
+    players: "1-4",
+    difficulty: "Easy",
+    
+    // Banner image URL
+    banner: "https://raw.githubusercontent.com/ScareTheVoices/D20/refs/heads/main/D20-Official%20Campaigns/ImSorryILoveYou/banner.png",
+    
+    // Story section (can include HTML)
+    story: `
+        <p>When Camelot falls, King Arthur perishes with it, and the realm dissolves into a silence that feels like a tomb. Years later, Merlin, the Witch who commands the unknown, reaches across time and resurrects a fallen king using a dangerous spell that invites chaos to seep into his flesh. Arthur Pendragon awakens with a vow: to raise a shattered kingdom from the ashes, he will wield chaos as a weapon, bending it to his iron will. Yet power corrupts, and his rebirth fractures him into something both sovereign and monstrous.</p>
+
+        <p>To anchor his rebirth of empire, the King of Chaos appoints seven Knights as his Seven Bridges Of Chaos, ruthless figures who stride between order and ruin, each a threat to the world's fragile balance. The once-great realm trembles as chaos leaks through the seams of civilization, and a destined band of heroes must rise to confront this apocalyptic design. Their quest demands courage, cunning, and a willingness to sacrifice everything they hold dear. For if they fail, the world will be consumed by Chaos and Camelot will be the only thing left to stand.</p>
+    `,
+    
+    // Key features
+    features: [
+        "Fairy tale-inspired narrative",
+        "Stat Modifier included in the campaign's Data file.",
+    ],
+    
+    // Campaign structure
+    chapters: [
+        {
+           
+        }
+    
+    ],
+    
+    // Resources and downloads
+    resources: [
+        {
+            name: "Play",
+            link: "https://scarethevoices.github.io/D20/D20-Official%20Campaigns/ShatteredTales/D20.html"
+        }
+    ]
+};
+
+window.campaignInfo = campaignInfo;
+
+
+
+
+
+
+
